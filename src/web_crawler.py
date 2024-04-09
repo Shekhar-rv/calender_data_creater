9b1d89f51df37643c1ea8ec5dc1b89c97835508c
Shekhar-rv <shekhar.rv@gmail.com>
2024-04-09 21:51:57 +0000

    Selenium changed

--- src/web_crawler.py
+++ src/web_crawler.py
@@ -1,29 +1,26 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 
 
 class WebCrawler:
-    def __init__(self, website_url: str, postcode: str, address_str: str, browser_path: str, driver_path: str, teardown: bool = True):
+    def __init__(self, website_url: str, postcode: str, address_str: str, teardown: bool = True):
         self.website_url = website_url
         self.postcode = postcode
         self.address_str = address_str
-        self.browser_path = browser_path
-        self.driver_path = driver_path
         self.teardown = teardown
-        print(f"browser_path: {self.browser_path} driver_path: {self.driver_path}")
-        options = Options()
-        options.binary_location = self.browser_path
-        # options.add_argument('--headless')  # type: ignore
-        # options.add_argument('--no-sandbox')  # type: ignore
-        # options.add_argument('--disable-dev-shm-usage')  # type: ignore
-        # Path to chromedriver driver on your system
-        chrome_driver_binary = Service(self.driver_path)
-        self.driver = webdriver.Chrome(service=chrome_driver_binary, options=options)  # Or you can use Chrome(), Safari(), etc.
-        # self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-        # self.driver = webdriver.Chrome(options=options)  # Or you can use Chrome(), Safari(), etc.
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_prefs = {}
+        chrome_options.experimental_options["prefs"] = chrome_prefs
+        chrome_prefs["profile.default_content_settings"] = {"images": 2}
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)  # Or you can use Chrome(), Safari(), etc.
         self.driver.get(website_url)
     
     def __enter__(self):
@@ -33,13 +30,14 @@ class WebCrawler:
         if self.teardown:
             self.driver.close()
 
-    def find_elements(self, element_name: str):
-        return self.driver.find_element_by_name(element_name)  # type: ignore
+    def find_elements(self, element_type: str, element_name: str):
+        return self.driver.find_element(element_type, element_name)  # type: ignore
     
     def run(self):
-        search_property = self.find_elements("search_property")  # type: ignore
-        print("search_property: ", search_property)
+        search_property = self.find_elements("name", "search_property")  # type: ignore
+        print(f"Is your postcode: {self.postcode} {type(self.postcode)}?")
+        search_property.clear()
         search_property.send_keys(self.postcode)  # type: ignore
-        search_property.send_keys(Keys.RETURN)  # type: ignore
+        search_property.submit()  # type: ignore
         assert "No results found." not in self.driver.page_source
         self.driver.close()
